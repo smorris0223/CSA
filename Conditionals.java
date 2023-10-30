87b78d61e68dc647f90d262d10e96a1ba5735370
Serena0223 <serenamorris@billericak12.com>
2023-10-30 13:59:10 -0400

    oct 30

---
+++ Conditionals.java
@@ -0,0 +1,14 @@
+public class Conditionals{
+    
+public boolean chimpTrouble(boolean aSmile, boolean bSmile){
+    if (aSmile== bSmile){
+        return true;
+    }
+    else { return false;}
+}
+
+public boolean negPos(int a, int b){
+    return false;
+}
+
+}
