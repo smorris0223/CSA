a7d1727a2b579f6a35b34309c48c78b19d28508c
smorris0223 <serenamorris@billericak12.com>
2023-12-08 13:56:50 -0500

    12-8-23

--- Factorials.java
+++ Factorials.java
@@ -1,24 +1,50 @@
 public class Factorials{
 
-  public int calcFactorial(int n){
-    int product=1;
+  public static long calcFactorial(int n){
+    long product=1;
     while(n>1){
-    product*=n;
-      n--;
+      product*=n;
+       n--;
     }
     return product;
   }
 
-public double calcE(){
-olde=1;
-x=1;
-e= ;
-while(e-olde<0.001){
-  e=olde+(1/calcFactorial(x));
-  x++;
-}  
-  return e;
-}  
+public static double calcE(){
+  double olde=0;
+  double e=1;
+  int x = 1;
+  while(e-olde>.001){
+    olde=e;
 
-public   
+    e+=(1.0/calcFactorial(x));
+    x++;
+  }
+  return e;
 }
+
+public static double calcEX(int x){
+  int n = 1;
+  double olde=0;
+  double e= 1;
+  while (e-olde>.001){
+    olde=e;
+    double num = Math.pow(x, n);
+    double den = calcFactorial(n);
+    e+=(num/den);//COULD all be done on one line but it found it looks really cluttered
+    n++;
+  }
+return e;
+}
+
+public static void main(String[] args) {
+  for(int x = 1; x<=20; x++){
+    System.out.println(calcFactorial(x));
+  }
+  System.out.printf("e is %2.3f \n", calcE());
+  for(int x = 1; x<=5; x++){
+    System.out.printf("e^" + x +"  is %2.3f \n", calcEX(x));
+  }
+}
+
+}
+
--- WhileLoops.java
+++ WhileLoops.java
@@ -1,5 +1,4 @@
 public class WhileLoops{
-
     
 public static int addOdds(int n){
     int sum = 0;
@@ -9,11 +8,18 @@ public static int addOdds(int n){
     return sum;
 }
 
-public int sumDigits(int n){
-    return 0;
-}
+public static int sumDigits(int n){
+    int sum= 0;
+    while(n>0){
+        int x=n%10;
+        sum+=x;
+        n/=10;
+    }
+    return sum;
+    }
 
-public int howManyYears(double startpop, double endpop){
+
+public static int howManyYears(double startpop, double endpop){
     int y = 0;
     while(startpop<endpop){
         y++;
@@ -22,25 +28,39 @@ public int howManyYears(double startpop, double endpop){
     return y;
 }
 
-public void printSum(int n){
-int x = 1;
-int y = 0;
-while (x<=n){
-System.out.print(x + " + ");
-y+=x;
-x++;
+public static void printSum(int n){
+    int x = 1;
+    int y = 0;
+    while (x<n){
+        System.out.print(x + " + ");
+        y+=x;
+        x++;
 }
-System.out.println("=" +y);
+System.out.println(n+" = " +(y+n));
 }
 
-public boolean isPerfectSquare(int n){
+public static boolean isPerfectSquare(int n){
     int x=1;
     while(x<n){
      x+=2;   
     }
     return x==n;
 }
-    public static void main(string[] args){
-        
-}
-}
+    public static void main(String[] args){
+       System.out.println(addOdds(5));
+       System.out.println(addOdds(6));
+       System.out.println(addOdds(10));
+       System.out.println(sumDigits(123));
+       System.out.println(sumDigits(4444));
+       System.out.println(sumDigits(221223));
+       System.out.println(howManyYears(111.2,120));
+       System.out.println(howManyYears(111.2,150));
+       printSum(10);
+       printSum(4);
+       printSum(7);
+       System.out.println(isPerfectSquare(25));
+       System.out.println(isPerfectSquare(49));
+       System.out.println(isPerfectSquare(10));
+       System.out.println(isPerfectSquare(14));
+}}
+
