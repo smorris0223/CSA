871a7a1cb2da2d939a39caa5adeae02e6eb55d46
smorris0223 <56898849+smorris0223@users.noreply.github.com>
2023-12-07 19:41:25 -0500

    Create WhileLoops.java
    12-7-23

---
+++ WhileLoops.java
@@ -0,0 +1,42 @@
+public class WhileLoops{
+
+public static int addOdds(int n){
+    int sum = 0;
+    while(n>=1){
+        if(n%2!=0){sum+=n;}
+    n--;}
+    return sum;
+}
+
+public int sumDigits(int n){
+    return 0;
+}
+
+public int howManyYears(double startpop, double endpop){
+    int y = 0;
+    while(startpop<endpop){
+        y++;
+        startpop+=(startpop*.0113);
+    }
+    return y;
+}
+
+public void printSum(int n){
+int x = 1;
+int y = 0;
+while (x<=n){
+System.out.print(x + " + ");
+y+=x;
+x++;
+}
+System.out.println("=" +y);
+}
+
+public boolean isPerfectSquare(int n){
+    int x=1;
+    while(x<n){
+     x+=2;   
+    }
+    return x==n;
+}
+}
